fbffc1184dd96bcf63c7c21ebecc7174096e88e6
seanavery <seanpollock3344@gmail.com>
2023-06-25 21:39:35 -0400

    init tests

---
+++ tests/test_ch340g.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ch340g.h"
+
+int main() {
+    // Perform your tests here
+    std::cout << "Running ch340g library tests..." << std::endl;
+
+    // ...
+
+    std::cout << "Tests completed." << std::endl;
+    return 0;
+}
