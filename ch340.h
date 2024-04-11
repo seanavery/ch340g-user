18232828069e695db6310cd272f8c9b1b0035189
seanavery <seanpollock3344@gmail.com>
2024-04-10 22:12:35 -0400

    Put dtr and rts in class

--- ch340.h
+++ ch340.h
@@ -7,20 +7,19 @@
 
 #define CTRL_IN (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_IN)
 #define CTRL_OUT (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_OUT)
-#define EP_DATA_OUT (0x2|LIBUSB_ENDPOINT_OUT)
-
-uint8_t dtr = 0;
-uint8_t rts = 0;
+#define EP_DATA_OUT (0x2 | LIBUSB_ENDPOINT_OUT)
 
 class CH340 {
     private:
+        uint8_t dtr = 0;
+        uint8_t rts = 0;
         libusb_context *ctx = NULL;
         libusb_device_handle *dev_handle;
     
     public:
         // USB driver
         bool connected;
-        int init(int vendor, int product, int baudrate=115200);
+        int init(int vendor, int product, int baudrate=BAUDRATE);
         int init_usb(int vendor, int product);
         int handshake();
         int set_baud(int baudrate);
