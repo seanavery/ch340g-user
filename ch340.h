dbc4a0d8126ee7f27849b73b29ba3940060fdc68
seanavery <seanpollock3344@gmail.com>
2024-04-10 21:38:31 -0400

    Clean up header

--- ch340.h
+++ ch340.h
@@ -8,6 +8,7 @@
 #define CTRL_IN (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_IN)
 #define CTRL_OUT (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_OUT)
 #define EP_DATA_OUT (0x2|LIBUSB_ENDPOINT_OUT)
+
 uint8_t dtr = 0;
 uint8_t rts = 0;
 
@@ -28,16 +29,3 @@ class CH340 {
         int bulk_read(unsigned char endpoint, unsigned char* data, int length, unsigned int timeout);
 
 };
-
-struct UsbDeviceInfo {
-    uint16_t vendorId;
-    uint16_t productId;
-    std::string description; // Optional: To store a device description
-};
-
-// Define the list of expected Vendor and Product IDs
-// std::vector<UsbDeviceInfo> expectedDevices = {
-//     {0x1a86, 0x7523, "QinHeng Electronics HL-340 USB-Serial adapter"},
-//     // Add more devices as needed
-// };
-
